426e75c54357e284cc2ca8b721a051278385536d
hritick <hritick.mallick@innoraft.com>
2025-08-12 10:47:08 +0530

    further enhancements done for the popup and icons for the tags changed.

--- JS/index.js
+++ JS/index.js
@@ -1,22 +1,15 @@
 $(document).ready(function () {
     // Open popup
     $(".generate-btn").click(function () {
-        $(".popup-search-engine-wrapper").fadeIn(300);
-        $(".popup-search-engine-wrapper").addClass('show');
+        $(".popup-search-engine-wrapper").slideDown(300).addClass('show');
         $("body").addClass("no-scroll");
     });
 
     // Close popup
     $(".popup-close-btn").click(function () {
-        $(".popup-search-engine-wrapper").fadeOut(300);
+        $(".popup-search-engine-wrapper").slideUp(300, function () {
+            $(this).removeClass('show');
+        });
         $("body").removeClass("no-scroll");
     });
-
-    // // Optional: close when clicking outside the popup container
-    // $(".popup-search-engine-wrapper").click(function (e) {
-    //     if ($(e.target).is(".popup-search-engine-wrapper")) {
-    //         $(this).fadeOut(300);
-    //         $("body").removeClass("no-scroll");
-    //     }
-    // });
 });
